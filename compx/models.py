e7f0503a90e8b5c1f7315d960d2867506da641ba
rajatbaheri <47448809+rajatbaheri@users.noreply.github.com>
2021-01-09 12:20:21 +0530

    Update models.py

--- compx/models.py
+++ compx/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
+from django.urls import reverse
+
 # Create your models here.
+
+class ModelWithFileField(models.Model):
+    fname_field = models.CharField(max_length=30,default='solve')
+    code_field = models.TextField(default="not found")
+    complexity_field = models.CharField(max_length=30,default='logarithmic')
+
+    complexity_key = models.CharField(max_length=30,default='exponential')
+    time1_field = models.CharField(max_length=30, default='10')
+    time2_field = models.CharField(max_length=30, default='20')
+    time3_field = models.CharField(max_length=30, default='30')
+
+    def get_absolute_url(self):
+        return reverse('compxdet',args=[str(self.id)])
